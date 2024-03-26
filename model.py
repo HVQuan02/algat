2f0efa8b7b379e44405357a7dd0e13b2534a5114
HVQuan02 <hovquan02@gmail.com>
2024-03-26 01:43:56 +0000

    debug model.py

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
-
+import pdb
 
 class GCNLayer(nn.Module):
     def __init__(self, in_feats, out_feats):
@@ -33,6 +33,7 @@ class GraphModule(nn.Module):
         self.gcn = nn.ModuleList(layers)
 
     def forward(self, x, device, get_adj=False):
+        pdb.set_trace()
         qx = self.wq(x)
         kx = self.wk(x)
         dot_mat = qx.matmul(kx.transpose(-1, -2))
